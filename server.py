2e1a8ade3bded91c1fc5c725c40787c701949c83
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-24 10:21:56 -0800

    licensed /home/arjun/work/programming/tmp/connect_4/server

--- server.py
+++ server.py
@@ -1,3 +1,22 @@
+#
+#  Connect 4
+#  A simple online connect-4 game in pygame.
+#  Copyright Arjun Sahlot 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 import socket, threading, pickle
 from board import Board
 from constants import HEADER, PORT, FORMAT, DISCONNECT
